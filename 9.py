8a5df4060c75895293ab3c4abe9e0ae8ef8c817f
Vedant <matevedant7@gmail.com>
2022-11-02 18:08:07 +0530

    updated

--- 9.py
+++ 9.py
@@ -8,14 +8,27 @@
 
 import numpy
 
-x = numpy.array([[1, 2], [4, 5]])
-y = numpy.array([[7, 8], [9, 10]])
+print(" Matrix : \n 1 | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9 \n")
+
+list1 = []
+list2 = []
+
+for i in range (0,9):
+    element = int(input(f'enter the element {i+1} of matrix 1 : '))
+    list1.append(element)
+
+for i in range (0,9):
+    element = int(input(f'enter the element {i+1} of matrix 2 : '))
+    list2.append(element)
+
+x = numpy.array([[list1[0],list1[1],list1[2]], [list1[3],list1[4],list1[5]], [list1[6],list1[7],list1[8]]])
+y = numpy.array([[list2[0],list2[1],list2[2]], [list2[3],list2[4],list2[5]], [list2[6],list2[7],list2[8]]])
 
 print("The element wise addition of matrix is : ")
-print(numpy.add(x, y))
+print(numpy.add(x,y))
 
 print("The element wise subtraction of matrix is : ")
-print(numpy.subtract(x, y))
+print(numpy.subtract(x,y))
 
 print ("The product of matrices is : ")
 print (numpy.dot(x,y))
