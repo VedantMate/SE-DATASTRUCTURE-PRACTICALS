611e9451a15a62aa577979c12b1da52e63d0b22e
Vedant <matevedant7@gmail.com>
2022-11-04 20:40:43 +0530

    updated

--- 7.py
+++ 7.py
@@ -1,4 +1,4 @@
-# Experiment No. 9 : Write a python Program for magic square. A magic square is an n * n matrix of the integers 1 to n2
+# Experiment No. 7 : Write a python Program for magic square. A magic square is an n * n matrix of the integers 1 to n2
 #                    such that the sum of each row, column, and diagonal is the same. The figure given below is an example
 #                    of magic square for case n=5. In this example, the common sum is 65.
 #                             15  8    1  24  17
